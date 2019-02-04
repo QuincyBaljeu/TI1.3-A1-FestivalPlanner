1a8b16a391ccc0582810ba17b0956074ce2282a7
Lucas Prins <lljPrins@avans.nl>
2019-02-04 12:23:25 +0100

    Added enum Genre, Eddited Artist
    Eddited Artist: constructor/atributes/get-setMethods

--- src/Data/Artist.java
+++ src/Data/Artist.java
@@ -1,4 +1,71 @@
 package Data;
 
+/**
+ * @author Lucas, Jasper
+ */
 public class Artist {
+    private String name;
+    private Enum<Genre> genre;
+    private String artistType;
+    private String filePathPFP; //PFP= Profile Picture
+    private String extraInformation;
+    private String country;      //Country
+
+    public Artist(String name, Enum<Genre> genre, String artistType, String filePathPFP, String extraInformation, String country) {
+        this.name = name;
+        this.genre = genre;
+        this.artistType = artistType;
+        this.filePathPFP = filePathPFP;
+        this.extraInformation = extraInformation;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Enum<Genre> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Enum<Genre> genre) {
+        this.genre = genre;
+    }
+
+    public String getArtistType() {
+        return artistType;
+    }
+
+    public void setArtistType(String artistType) {
+        this.artistType = artistType;
+    }
+
+    public String getFilePathPFP() {
+        return filePathPFP;
+    }
+
+    public void setFilePathPFP(String filePathPFP) {
+        this.filePathPFP = filePathPFP;
+    }
+
+    public String getExtraInformation() {
+        return extraInformation;
+    }
+
+    public void setExtraInformation(String extraInformation) {
+        this.extraInformation = extraInformation;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
+
---
+++ src/Data/Genre.java
@@ -0,0 +1,7 @@
+package Data;
+//todo Add more genres
+//https://research.google.com/audioset/ontology/music_genre_1.html
+
+public enum Genre {
+    ROCK, PUNK, POP, CLASIC, NEWAGE
+}
