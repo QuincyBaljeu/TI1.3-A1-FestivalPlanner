eec50acbf60701acee9cec796b5f06b9e0c8ee7e
Lucas Prins <lljPrins@avans.nl>
2019-02-04 12:29:16 +0100

    Eddited Stage
    Eddited Stage: constructor/atributes/get-set Methods

--- src/Data/Stage.java
+++ src/Data/Stage.java
@@ -1,4 +1,32 @@
 package Data;
 
+import java.util.List;
+
+/**
+ * @author Lucas, Jasper
+ */
 public class Stage {
+    private String name;
+    private List<Performance> performances;
+
+    public Stage(String name, List<Performance> performances) {
+        this.name = name;
+        this.performances = performances;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Performance> getPerformances() {
+        return performances;
+    }
+
+    public void setPerformances(List<Performance> performances) {
+        this.performances = performances;
+    }
 }
