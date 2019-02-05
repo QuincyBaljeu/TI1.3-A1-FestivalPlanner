a6556985a913c3742b8b9fd9307213e7b4ed2092
Lucas Prins <lljPrins@avans.nl>
2019-02-05 13:40:09 +0100

    Added FestivalDay
    Saves all stages and has a local date

--- src/Data/FestivalDay.java
+++ src/Data/FestivalDay.java
@@ -1,4 +1,26 @@
 package Data;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FestivalDay {
+
+    private List<Stage> stages;
+    private LocalDate date;
+
+
+    public FestivalDay(LocalDate date) {
+        this.date = date;
+        this.stages = new ArrayList<>();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
