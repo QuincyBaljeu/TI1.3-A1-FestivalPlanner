3d552a72e7d93e8f07cd37d7367e6ccfbd6ec4ff
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2019-02-24 21:57:37 +0100

    Added getters to the TileMap Subclasses

--- src/Data/Tiled/TileMap.java
+++ src/Data/Tiled/TileMap.java
@@ -157,41 +157,81 @@ public class TileMap {
         private int x;
         private int y;
 
-        public void setData(int[] data) {
+		public int[] getData() {
+			return data;
+		}
+
+		public void setData(int[] data) {
             this.data = data;
         }
+
+		public int getHeight() {
+			return height;
+		}
 
         public void setHeight(int height) {
             this.height = height;
         }
 
+		public int getId() {
+			return id;
+		}
+
         public void setId(int id) {
             this.id = id;
         }
+
+		public String getName() {
+			return name;
+		}
 
         public void setName(String name) {
             this.name = name;
         }
 
+		public int getOpacity() {
+			return opacity;
+		}
+
         public void setOpacity(int opacity) {
             this.opacity = opacity;
         }
+
+		public String getType() {
+			return type;
+		}
 
         public void setType(String type) {
             this.type = type;
         }
 
+		public boolean isVisible() {
+			return visible;
+		}
+
         public void setVisible(boolean visible) {
             this.visible = visible;
         }
+
+		public int getWidth() {
+			return width;
+		}
 
         public void setWidth(int width) {
             this.width = width;
         }
 
+		public int getX() {
+			return x;
+		}
+
         public void setX(int x) {
             this.x = x;
         }
+
+		public int getY() {
+			return y;
+		}
 
         public void setY(int y) {
             this.y = y;
@@ -202,11 +242,19 @@ public class TileMap {
         private int firstgid;
         private String source;
 
+		public int getFirstgid() {
+			return firstgid;
+		}
+
         public void setFirstgid(int firstgid) {
             this.firstgid = firstgid;
         }
 
-        public void setSource(String source) {
+		public String getSource() {
+			return source;
+		}
+
+		public void setSource(String source) {
             this.source = source;
         }
     }
