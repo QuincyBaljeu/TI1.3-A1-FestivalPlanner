3597c7606510acf55e5605a1ba1ad1421e2ba478
Quincy Baljeu <q.baljeu@gmail,com>
2019-03-11 18:22:59 +0100

    changed tileset to json

--- Resources/TestData/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="tilemap" tilewidth="16" tileheight="16" tilecount="117" columns="13">
- <image source="../Downloads/tilemap.png" width="208" height="144"/>
-</tileset>
