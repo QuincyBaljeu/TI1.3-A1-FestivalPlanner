4e4ad51df12ce371152fc1e30183fae332a811a2
Quincy Baljeu <q.baljeu@gmail,com>
2019-03-11 18:29:35 +0100

    added tilemap images

--- Resources/TestData/stages.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="stages" tilewidth="16" tileheight="16" tilecount="3328" columns="64">
- <image source="../Downloads/LPC Submissions/Interior 2.png" width="1024" height="832"/>
-</tileset>
