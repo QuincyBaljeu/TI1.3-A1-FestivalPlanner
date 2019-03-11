b477562f27cc63d1b7af75e131b7eefa4d8af769
Quincy Baljeu <q.baljeu@gmail,com>
2019-03-11 14:23:20 +0100

    Added festival map to resources

---
+++ Resources/TestData/Terrain and Outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Terrain and Outside" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../Downloads/LPC Submissions/Terrain and Outside.png" width="1024" height="1024"/>
+</tileset>
