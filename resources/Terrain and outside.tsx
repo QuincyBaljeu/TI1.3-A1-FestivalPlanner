620d2ad252166af53c658680b4602f6bb69b095f
Quincy Baljeu <q.baljeu@gmail,com>
2019-03-18 13:44:06 +0100

    Added festival resources

---
+++ resources/Terrain and outside.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Terrain and outside" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="Terrain and Outside.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="FestivalGround" tile="35"/>
+ </terraintypes>
+ <tile id="79" terrain="0,0,0,"/>
+ <tile id="80" terrain="0,0,,0"/>
+ <tile id="143" terrain="0,,0,0"/>
+ <tile id="144" terrain=",0,0,0"/>
+ <tile id="268" terrain=",,,0"/>
+ <tile id="271" terrain=",,0,0"/>
+ <tile id="273" terrain=",,0,"/>
+ <tile id="332" terrain=",0,,0"/>
+ <tile id="398" terrain="0,0,0,0"/>
+ <tile id="401" terrain="0,,0,"/>
+ <tile id="588" terrain=",0,,"/>
+ <tile id="591" terrain="0,0,,"/>
+ <tile id="593" terrain="0,,,"/>
+</tileset>
---
+++ resources/tilemap.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tilemap" tilewidth="16" tileheight="16" tilecount="117" columns="13">
+ <image source="tilemap.png" width="208" height="144"/>
+ <terraintypes>
+  <terrain name="Path" tile="35"/>
+ </terraintypes>
+ <tile id="11" terrain=",0,0,0"/>
+ <tile id="12" terrain="0,,0,0"/>
+ <tile id="21" terrain=",,,0"/>
+ <tile id="22" terrain=",,0,0"/>
+ <tile id="23" terrain=",,0,"/>
+ <tile id="24" terrain="0,0,,0"/>
+ <tile id="25" terrain="0,0,0,"/>
+ <tile id="34" terrain=",0,,0"/>
+ <tile id="35" terrain="0,0,0,0"/>
+ <tile id="36" terrain="0,,0,"/>
+ <tile id="37" terrain="0,,,"/>
+ <tile id="38" terrain=",0,,"/>
+ <tile id="47" terrain=",0,,"/>
+ <tile id="48" terrain="0,0,,"/>
+ <tile id="49" terrain="0,,,"/>
+ <tile id="50" terrain=",,0,"/>
+ <tile id="51" terrain=",,,0"/>
+</tileset>
