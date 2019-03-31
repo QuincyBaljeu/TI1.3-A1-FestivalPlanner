56b500cdb847dd6a7815f1b048d89a63b2eeef09
Lucas Prins <lljPrins@avans.nl>
2019-03-31 15:16:15 +0200

    Flow_Layer class generates a disstance map

--- src/Data/Tiled/Layer/Flow_Layer.java
+++ src/Data/Tiled/Layer/Flow_Layer.java
@@ -16,54 +16,81 @@ public class Flow_Layer {
 
     public Flow_Layer(TileLayer collisionLayer, TiledObject goal, int[][] data2D) {
         this.data2D = data2D;
-
         this.endPoint = goal;
-
         generateDistMap(collisionLayer);
 
     }
 
     private void generateDistMap(TileLayer collisionLayer) {
         this.distMap = new int[collisionLayer.getWidth()][collisionLayer.getHeight()];
+        resetDistanceMap();
 
-        Queue<Integer[][]> frontier = new LinkedList<>();
-        frontier.offer(new Integer[endPoint.getX()][endPoint.getY()]);
-        startTile.setDistCost(0);
+        int centerX = (int)this.endPoint.getX()/32;
+        int centerY = (int)this.endPoint.getY()/32;
+
+        Queue<Point2D> frontier = new LinkedList<>();
+        frontier.offer(new Point2D(centerX, centerY));
+        this.distMap[centerX][centerY] = 0;
 
         while (!frontier.isEmpty()) {
-            int current = frontier.peek();
+            Point2D current = frontier.peek();
             frontier.poll();
 
-            for (Tile neighbor : getNeighbors(current)) {
+            for (Point2D neighbor : getNeighbors(current)) {
                 frontier.add(neighbor);
-                neighbor.setDistCost(current.getDistCost() + 1);
-
+                this.distMap[(int)neighbor.getX()][(int)neighbor.getY()] = this.distMap[(int)current.getX()][(int)current.getY()] + 1;
             }
         }
-        for (int x = 0; x < this.tiles.length; x++) {
-            for (int y = 0; y < this.tiles[0].length; y++) {
-                if (this.tiles[x][y].isAcesAble()) {
-                    ArrayList<Integer> leftRight = getLeftRight(tiles[x][y]);
-                    ArrayList<Integer> topBot = getTopBot(tiles[x][y]);
-                    double flowX = leftRight.get(0) - leftRight.get(1);
-                    double flowY = topBot.get(0) - topBot.get(1);
-                    if (this.tiles[x][y].getDistCost() != 0)
-                        this.tiles[x][y].setFlow(new java.awt.geom.Point2D.Double(flowX, flowY));
-                }
+/*-------------------printing map
+//        for (int[] ints : this.distMap) {
+//            for (int anInt : ints) {
+//                System.out.print(anInt);
+//            }
+//            System.out.println("");
+//        }
+*/
+
+//        for (int x = 0; x < this.tiles.length; x++) {
+//            for (int y = 0; y < this.tiles[0].length; y++) {
+//                if (this.tiles[x][y].isAcesAble()) {
+//                    ArrayList<Integer> leftRight = getLeftRight(tiles[x][y]);
+//                    ArrayList<Integer> topBot = getTopBot(tiles[x][y]);
+//                    double flowX = leftRight.get(0) - leftRight.get(1);
+//                    double flowY = topBot.get(0) - topBot.get(1);
+//                    if (this.tiles[x][y].getDistCost() != 0)
+//                        this.tiles[x][y].setFlow(new java.awt.geom.Point2D.Double(flowX, flowY));
+//                }
+//            }
+//        }
+    }
+
+    private void resetDistanceMap() {
+        for (int x = 0; x < this.data2D.length; x++) {
+            for (int y = 0; y < this.data2D[0].length; y++) {
+                this.distMap[x][y] = -1;
             }
         }
     }
 
-    private ArrayList<Integer[][]> getNeighbors(Integer[][] center) {
-        ArrayList<Integer[][]> neighbors = new ArrayList<>();
-        for (int x = center[0].length - 1; x <= center[0].length + 1; x++) {
-            if (x >= 0 && x < this.getSize().getX()) {
-                for (int y = tile.getY() - 1; y <= tile.getY() + 1; y++)
-                    if (y >= 0 && y < this.getSize().getY() && tiles[x][y].isAcesAble() && tiles[x][y] != tile && tiles[x][y].getDistCost() == -1) {
-                        neighbors.add(tiles[x][y]);
+    private  ArrayList<Point2D> getNeighbors(Point2D center) {
+        ArrayList<Point2D> neighbors = new ArrayList<>();
+
+        int centerX = (int)center.getX();
+        int centerY = (int)center.getY();
+
+        for (int x = centerX - 1; x <= centerX + 1; x++) {
+            if (x >= 0 && x < data2D.length) {
+                for (int y = centerY - 1; y <= centerY + 1; y++)
+                    if (y >= 0 && y < data2D[0].length && data2D[x][y] == 0 && !(centerX == x && centerY == y)) {
+                        if (this.distMap[x][y] == -1) {
+                            System.out.println(x + "<-x y->" + y + "  |  " + centerX + "<-x y->" + centerY);
+                            neighbors.add(new Point2D(x, y));
+                        }
+
                     }
             }
         }
+
         return neighbors;
     }
 
